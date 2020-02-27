46d16f97bb01843c2418a86b8fd46db9e90c0d24
mohammed <mdrame113@gmail.com>
2020-02-27 13:23:21 -0800

    changes made in app.py

--- app.py
+++ app.py
@@ -9,28 +9,28 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # display only 
-    return render_template('index.html') #home page
+    return render_template("index.html") #home page
     
 
 # gallery route 
 @app.route('/gallery')
 def gallery():
     # this route display kobi gallery of photographs. Universal
-    return render_template('gallery.html') #home page
+    return render_template("gallery.html") #home page
     
 
 # highlights 
 @app.route('/highlights')
 def highlights():
      # this route holds kobe highlights ( videos )
-    return render_template('highlights.html') #home page
+    return render_template("highlights.html") #home page
     
 
 
 
 
 # main module page 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
 
 # Todo:

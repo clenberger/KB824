7cd021c4f8c3ef4e5a3d0ce9fbc1dcb4afb89b19
JericHunter <jeric.hunterr@gmail.com>
2020-03-04 02:23:25 -0800

    doin herokuv2

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
-from flask import Flask, render_template, redirect, request, url_for
-import requests
-import os 
+from flask import Flask, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
 
-# home route 
 @app.route('/')
 def index():
-    # display only 
-    return render_template("index.html") #home page
-    
+    """Return homepage."""
+    return render_template('index.html')
 
-# gallery route 
 @app.route('/gallery')
 def gallery():
-    # this route display kobi gallery of photographs. Universal
-    return render_template("gallery.html") #home page
-    
+    """Return gallery."""
+    return render_template('gallery.html')
 
-# highlights 
 @app.route('/highlights')
-def highlights():
-     # this route holds kobe highlights ( videos )
-    return render_template("highlights.html") #home page
-    
-
-
-
-
-# main module page 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-# Todo:
-# First Attempt fail
+def highlight():
+    """Return highlights."""
+    return render_template('highlights.html')
